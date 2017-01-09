66fcaf3cf61ece97997179dfb5fdd39ac75b52a4
aryashree <aryashree.pritikrishna@mindtree.com>
2017-01-09 08:26:44 +0530

    Initial commit

---
+++ test/components/Results_spec.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithClass,
+  Simulate
+} from 'react-addons-test-utils';
+import {List, Map} from 'immutable';
+import {Results} from '../../src/components/Results';
+import {expect} from 'chai';
